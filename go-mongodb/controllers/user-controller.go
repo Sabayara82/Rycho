96c559eb52400e9655eb9511288f898278dcf56b
mahianm <101750087+mahianm@users.noreply.github.com>
2024-03-06 02:57:47 -0700

    Added functions to the user controller and routes.

--- go-mongodb/controllers/user-controller.go
+++ go-mongodb/controllers/user-controller.go
@@ -57,3 +57,168 @@ func CreateUser() gin.HandlerFunc {
 		c.JSON(http.StatusCreated, responses.UserResponse{Status: http.StatusCreated, Message: "success", Data: map[string]interface{}{"data": result}})
 	}
 }
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// Get user ID from request parameters
+		userID := c.Param("id")
+
+		// Convert user ID string to ObjectID
+		objID, err := primitive.ObjectIDFromHex(userID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": "Invalid user ID"}})
+			return
+		}
+
+		var user models.User
+		err = userCollection.FindOne(ctx, bson.M{"_id": objID}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusNotFound, responses.UserResponse{Status: http.StatusNotFound, Message: "error", Data: map[string]interface{}{"data": "User not found"}})
+			return
+		}
+
+		c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": user}})
+	}
+}
+
+func UpdateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// Get user ID from request parameters
+		userID := c.Param("id")
+
+		// Convert user ID string to ObjectID
+		objID, err := primitive.ObjectIDFromHex(userID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": "Invalid user ID"}})
+			return
+		}
+
+		var user models.User
+		err = userCollection.FindOne(ctx, bson.M{"_id": objID}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusNotFound, responses.UserResponse{Status: http.StatusNotFound, Message: "error", Data: map[string]interface{}{"data": "User not found"}})
+			return
+		}
+
+		var updatedUser models.User
+		if err := c.BindJSON(&updatedUser); err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+
+		// Update user fields if provided in request
+		if updatedUser.Name != "" {
+			user.Name = updatedUser.Name
+		}
+		if updatedUser.EmailAddress != "" {
+			user.EmailAddress = updatedUser.EmailAddress
+		}
+		if updatedUser.Password != "" {
+			user.Password = updatedUser.Password
+		}
+
+		_, err = userCollection.ReplaceOne(ctx, bson.M{"_id": objID}, user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+
+		c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": "User updated successfully"}})
+	}
+}
+
+func SearchUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		query := c.Query("q")
+
+		filter := bson.M{
+			"$or": []bson.M{
+				bson.M{"name": bson.M{"$regex": primitive.Regex{Pattern: query, Options: "i"}}},         // Case-insensitive partial match on name
+				bson.M{"emailaddress": bson.M{"$regex": primitive.Regex{Pattern: query, Options: "i"}}}, // Case-insensitive partial match on email
+			},
+		}
+
+		// Perform the search operation in the database
+		cursor, err := userCollection.Find(ctx, filter)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+		defer cursor.Close(ctx)
+
+		// Iterate through the results and collect them in a slice
+		var users []models.User
+		for cursor.Next(ctx) {
+			var user models.User
+			if err := cursor.Decode(&user); err != nil {
+				c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+				return
+			}
+			users = append(users, user)
+		}
+
+		// Return the search results in the response
+		c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": users}})
+	}
+}
+
+func DeleteUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// Extract user ID from the request parameters
+		userID := c.Param("id")
+
+		// Convert user ID string to ObjectID
+		objID, err := primitive.ObjectIDFromHex(userID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": "Invalid user ID"}})
+			return
+		}
+
+		// Delete the user from the database
+		_, err = userCollection.DeleteOne(ctx, bson.M{"_id": objID})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+
+		c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": "User deleted successfully"}})
+	}
+}
+
+func ListUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		cursor, err := userCollection.Find(ctx, bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+		defer cursor.Close(ctx)
+
+		var users []models.User
+		for cursor.Next(ctx) {
+			var user models.User
+			if err := cursor.Decode(&user); err != nil {
+				c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+				return
+			}
+			users = append(users, user)
+		}
+
+		c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": users}})
+	}
+}
--- go-mongodb/routes/user-routes.go
+++ go-mongodb/routes/user-routes.go
@@ -7,5 +7,11 @@ import (
 )
 
 func UserRoute(router *gin.Engine) {
+	// User Routes
 	router.POST("/users", controllers.CreateUser())
+	router.GET("/users/:id", controllers.GetUser())
+	router.PUT("/users/:id", controllers.UpdateUser())
+	router.GET("/users/search", controllers.SearchUsers())
+	router.DELETE("/users/:id", controllers.DeleteUser())
+	router.GET("/users", controllers.ListUsers())
 }
