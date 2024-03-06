8b4a0b49439d9b5959803813859722d4d138858d
mahianm <101750087+mahianm@users.noreply.github.com>
2024-03-06 01:08:28 -0700

    Update user-models.go to use singleton design pattern and added struct fields

--- go-mongodb/models/user-models.go
+++ go-mongodb/models/user-models.go
@@ -1,10 +1,33 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"sync"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var (
+	instance *User
+	once     sync.Once
+)
+
+func GetUserInstance() *User {
+	once.Do(func() {
+		instance = &User{}
+	})
+	return instance
+}
 
 type User struct {
-	Id           primitive.ObjectID `json:"id,omitempty"`
-	Name         string             `json:"name,omitempty" validate:"required"`
-	EmailAddress string             `json:"emailaddress,omitempty" validate:"required"`
-	Password     string             `json:"password,omitempty" validate:"required"`
+	Id             primitive.ObjectID `json:"id,omitempty"`
+	Name           string             `json:"name,omitempty" validate:"required"`
+	EmailAddress   string             `json:"emailaddress,omitempty" validate:"required"`
+	Password       string             `json:"password,omitempty" validate:"required"`
+	ProfilePicture string             `json:"profilepicture,omitempty"`
+	Friends        []User             `json:"friends,omitempty"`
+	Followers      []User             `json:"followers,omitempty"`
+}
+
+func NewUser() *User {
+	return GetUserInstance()
 }
