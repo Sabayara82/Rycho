19f5665e29c34db58bbe5708352d94eec9cf1bb4
zoraiz2000 <zoraizkhan2000@gmail.com>
2024-03-14 21:16:39 -0600

    display spotify profile name & profile image

--- nextjs-blog/app/profile/page.tsx
+++ nextjs-blog/app/profile/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
-import React, { useEffect } from 'react';
+import axios from "axios";
 import Image from "next/image";
+import React, { useEffect } from 'react';
+
 
 export default function Home() {
 
-  const [token, setToken] = React.useState("");
+  const [token, setToken] = React.useState<string | null>(null);
+  const [userName, setUserName] = React.useState<string | null>(null);
+  const [userImage, setUserImage] = React.useState<string | null>(null);
 
   useEffect(() => {
     const hash = window.location.hash
@@ -19,19 +23,45 @@ export default function Home() {
         
     }
 
-    setToken(storedToken ?? "")
+    setToken(storedToken)
   }, [])  
+
+  useEffect(() => {
+    if (token) {
+      fetchUserProfile();
+    }
+  }, [token]);
+
+  const fetchUserProfile = async () => {
+    const {data} = await axios.get('https://api.spotify.com/v1/me', {
+      headers: {
+        Authorization: 'Bearer ' + token
+      }
+    })
+
+    console.log(data)
+    setUserName(data.display_name)
+    if (data.images && data.images.length > 0) {
+      setUserImage(data.images[1].url);
+      console.log("here")
+    } else {
+      console.log("not here")
+    }
+  }
+  
 
   return (
     <div className="flex min-h-screen flex-col bg-[#121212] mt-20 max-w-7xl min-h-96 mx-auto rounded-3xl p-10">
         <h1 className="text-4xl font-semibold text-center mb-8">Profile</h1>
         <Image className="bg-[#ffffff] mx-auto rounded-full mb-3"
-            src="/user.png"
+            src={userImage || "/user.png"}
             alt="image not found"
             width={128} 
             height={128} 
+            priority
           />
-        <h3 className="text-sm text-center mb-3">John Doe</h3>
+        
+        <h3 className="text-sm text-center mb-3">{userName || ("")}</h3>
         <h3 className="text-center font-semibold mb-1">1k Followers | 100 Following</h3>
         <h3 className="text-center font-semibold mb-8">Monthly Listeners 10k</h3>
         <button className="font-semibold mx-auto max-w-fit bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-500 rounded-full py-2 px-6 mb-12 focus:outline-none">
