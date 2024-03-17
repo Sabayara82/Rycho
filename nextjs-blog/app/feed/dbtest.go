3bda4231113344bda0820c296472830e20518ff1
nessmamd <113803210+nessmamd@users.noreply.github.com>
2024-03-17 11:16:43 -0600

    zahwa mdoif

--- nextjs-blog/app/feed/dbtest.go
+++ nextjs-blog/app/feed/dbtest.go
@@ -12,17 +12,23 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type currentUserDetails struct {
+	id           int32
+	userName     string
+	followingIds []int32
+}
+
 type FeedItem struct {
-	ID              primitive.ObjectID `bson:"_id,omitempty"`
-	UserID          primitive.ObjectID `bson:"userId,omitempty"`
-	AlbumName       string             `bson:"albumName,omitempty"`
-	Song            string             `bson:"song,omitempty"`
-	AlbumURL        string             `bson:"albumUrl,omitempty"`
-	CreatedAt       int32              `bson:"createdAt,omitempty"`
-	Caption         string             `bson:"caption,omitempty"`
-	Likes           int32              `bson:"likes,omitempty"`
-	RoomStat        bool               `bson:"roomStat,omitempty"`
-	ArrOfCommentIDs []int32            `bson:"arrOfCommentIds,omitempty"`
+	ID              int32   `bson:"_id,omitempty"`
+	UserID          int32   `bson:"userId,omitempty"`
+	AlbumName       string  `bson:"albumName,omitempty"`
+	Song            string  `bson:"song,omitempty"`
+	AlbumURL        string  `bson:"albumUrl,omitempty"`
+	CreatedAt       int32   `bson:"createdAt,omitempty"`
+	Caption         string  `bson:"caption,omitempty"`
+	Likes           int32   `bson:"likes,omitempty"`
+	RoomStat        bool    `bson:"roomStat,omitempty"`
+	ArrOfCommentIDs []int32 `bson:"arrOfCommentIds,omitempty"`
 }
 
 // the collection of comments
@@ -34,7 +40,7 @@ type Comment struct {
 	NumberOfLikes int32              `bson:"numberOfLikes,omitempty"`
 }
 
-func main() {
+func maintest() *mongo.Client {
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI("mongodb+srv://root:rycho123@cluster0.dk3gj4m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
@@ -68,42 +74,46 @@ func main() {
 	}
 	defer client.Disconnect(ctx)
 
-	feedDatabase := client.Database("feed")
-	postsCollection := feedDatabase.Collection("posts")
-	commentsCollection := feedDatabase.Collection("comments")
+	// feedDatabase := client.Database("feed")
+	// postsCollection := feedDatabase.Collection("posts")
+	// commentsCollection := feedDatabase.Collection("comments")
 
-	item := FeedItem{
-		ID:              primitive.NewObjectID(),
-		UserID:          primitive.NewObjectID(),
-		AlbumName:       "Random Album",
-		Song:            "Random Song",
-		AlbumURL:        "http://example.com/random_album",
-		CreatedAt:       3, // Assuming this is a placeholder, use actual timestamp if available
-		Caption:         "A random caption",
-		Likes:           100,
-		RoomStat:        true,
-		ArrOfCommentIDs: []int32{1, 2, 3},
-	}
+	// item := FeedItem{
+	// 	ID:              1,
+	// 	UserID:          32,
+	// 	AlbumName:       "Random Album",
+	// 	Song:            "Random Song",
+	// 	AlbumURL:        "http://example.com/random_album",
+	// 	CreatedAt:       3, // Assuming this is a placeholder, use actual timestamp if available
+	// 	Caption:         "A random caption",
+	// 	Likes:           100,
+	// 	RoomStat:        true,
+	// 	ArrOfCommentIDs: []int32{1, 2, 3},
+	// }
 
-	postsResult, err := postsCollection.InsertOne(ctx, item)
-	if err != nil {
-		fmt.Println("Error inserting document:", err)
-		return
-	}
+	// postsResult, err := postsCollection.InsertOne(ctx, item)
+	// if err != nil {
+	// 	fmt.Println("Error inserting document:", err)
+	// 	return
+	// }
 
-	theComment := Comment{
-		ID:            primitive.NewObjectID(),
-		UserID:        primitive.NewObjectID(),
-		Comment:       "such a sick song i love it",
-		CreatedAt:     5,
-		NumberOfLikes: 5,
-	}
-	commentResult, err := commentsCollection.InsertOne(ctx, theComment)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Inserted %v documents into comments collection!\n", commentResult.InsertedID)
-	fmt.Printf("Inserted %v documents into episode collection!\n", commentResult)
-	fmt.Printf("Inserted %v documents into episode collection!\n", postsResult)
+	// theComment := Comment{
+	// 	ID:            primitive.NewObjectID(),
+	// 	UserID:        primitive.NewObjectID(),
+	// 	Comment:       "such a sick song i love it",
+	// 	CreatedAt:     5,
+	// 	NumberOfLikes: 5,
+	// }
+	// commentResult, err := commentsCollection.InsertOne(ctx, theComment)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Inserted %v documents into comments collection!\n", commentResult.InsertedID)
+	// fmt.Printf("Inserted %v documents into episode collection!\n", commentResult)
+	// fmt.Printf("Inserted %v documents into episode collection!\n", postsResult)
+
+	return client
 
 }
+
+var DB *mongo.Client = maintest()
