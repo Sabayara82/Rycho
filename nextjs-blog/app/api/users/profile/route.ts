95e22be424f21c53006ac22f20037e3f628dde15
IshaHaider <66149883+IshaHaider@users.noreply.github.com>
2024-03-21 00:37:20 -0600

    changed UserModel to userModel

--- nextjs-blog/app/api/users/profile/route.ts
+++ nextjs-blog/app/api/users/profile/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import {connect} from "@/dbConfig/dbConfig";
-import User from "@/models/UserModel";
+import User from "@/models/userModel";
 
 
 export async function POST(request: NextRequest) {
