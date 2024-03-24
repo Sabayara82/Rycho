f11c503d2ed0c4eeb9965fd637a724556ad777f8
nessmamd <113803210+nessmamd@users.noreply.github.com>
2024-03-24 00:13:27 -0600

    left is the dates

--- nextjs-blog/app/feed/[id]/page.tsx
+++ nextjs-blog/app/feed/[id]/page.tsx
@@ -12,7 +12,8 @@ export default function Feed({params} : {params: {id: string}}) {
     const [spotifyId, setId] = useState<string | null>(null);
     const [userName, setUserName] = useState<string | null>(null);
     const [userImage, setUserImage] = useState<string | null>(null);  
-    const [followingIds, setFollowingIds] = useState<number[]>([]);
+    const [followingIds, setFollowingIds] = useState<string[]>([]);
+    const [posts, setPosts] = useState<any[]>([]);
 
   
     useEffect(() => {
@@ -25,20 +26,53 @@ export default function Feed({params} : {params: {id: string}}) {
     useEffect(() => {
       if (token) {
         fetchMyFollowing()
+        retriveThePosts()
       }
-    }, [token]);
+    }, [token, followingIds, posts]);
 
 
     const fetchMyFollowing = async () => {
-        try {
-          const { data } = await axios.get(`http://localhost:3000/api/users/profile?action=getFollowing&spotifyId=${spotifyId}`);      
-          const {following} = data 
-          console.log("Following IDs:", following);
-        //   lets display something here to see it on the page
-        } catch (error) {
-          console.error("Error fetching followers count: ", error);
+      try {
+        const { data } = await axios.get(`http://localhost:3000/api/users/profile?action=getFollowing&spotifyId=${spotifyId}`);
+        const { following } = data;
+        const newFollowingIds = []; // Create a new array to collect Spotify IDs
+
+        //you loop through every index and get the spotifyID
+        for(let x = 0; x < following.length; x++){
+          console.log(following[x].spotifyId)
+          newFollowingIds.push(following[x].spotifyId)
+        } 
+
+        setFollowingIds(newFollowingIds)
+        console.log(followingIds)
+      } catch (error) {
+        console.error("Error fetching followers count: ", error);
+      }
+    }
+
+    const retriveThePosts = async () => {
+      const allOfThePosts = []
+      for(let p = 0; p < followingIds.length; p++){
+        try{
+          const response = await axios.get(
+            `http://localhost:3000/api/feed/post/?spotifyId=${followingIds[p]}`,
+            {
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            }
+          );
+          const { allPosts } = response.data;
+          console.log(posts)
+          allOfThePosts.push(...allPosts);
+        }
+        catch(error){
+          console.error("Error fetching user's posts': ", error);
         }
       }
+      
+      setPosts(allOfThePosts);
+    }
   
       return (
         <>HELLO TEST</>
