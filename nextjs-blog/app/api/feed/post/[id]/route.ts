2ee61876144369cb2a0875c146da426f1a461b27
nessmamd <113803210+nessmamd@users.noreply.github.com>
2024-03-25 02:18:45 -0600

    changes on view, model, and controller except like

--- nextjs-blog/app/api/feed/post/[id]/route.ts
+++ nextjs-blog/app/api/feed/post/[id]/route.ts
@@ -32,3 +32,25 @@ export async function GET(request: NextRequest, {params} : {params: {id: string}
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
+
+// try update instead
+export async function PUT(request: NextRequest) {
+    const url = new URL(request.url);
+    const id = url.searchParams.get("id");
+    const likes = url.searchParams.get("likes");
+    // const {postID, likes} = reqBody; 
+    // const thePost = await Post.findOne({ spotifyId: id });
+    const thePost = await Post.findByIdAndUpdate(id, { likes }, { new: true });
+
+    if (!thePost) {
+        return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    thePost.likes = likes; 
+    await thePost.save();  
+    return NextResponse.json({
+            message: "Like count has been updated", 
+            success: true 
+        }); 
+} 
+
--- nextjs-blog/app/feed/[id]/page.tsx
+++ nextjs-blog/app/feed/[id]/page.tsx
@@ -51,8 +51,10 @@ export default function Feed({params} : {params: {id: string}}) {
 
     const retriveThePosts = async () => {
       const allOfThePosts = []
+      const noLikes = []
       for(let p = 0; p < followingIds.length; p++){
         try{
+          
           const response = await axios.get(
             `http://localhost:3000/api/feed/post/?spotifyId=${followingIds[p]}`,
             {
@@ -63,6 +65,7 @@ export default function Feed({params} : {params: {id: string}}) {
           );
           const { allPosts } = response.data;
           allOfThePosts.push(...allPosts);
+          noLikes.push(false); 
         }
         catch(error){
           console.error("Error fetching user's posts': ", error);
@@ -72,9 +75,9 @@ export default function Feed({params} : {params: {id: string}}) {
         }
       }
   
-      const sortedPosts = allOfThePosts.sort((a, b) => b.updatedAt - a.updatedAt);
+      const sortedPosts = allOfThePosts.sort((a, b) => a.updatedAt - b.updatedAt).reverse();
       setPosts(sortedPosts);
-      console.log(sortedPosts)
+      setlikingInteraction(noLikes); 
     }
 
     // creating the function that gets the userDetails based off of the ID
@@ -85,7 +88,6 @@ export default function Feed({params} : {params: {id: string}}) {
           `http://localhost:3000/api/users/profile/?action=getAllUsers`
         )
         let temp = userInfo.data.allUsers
-        console.log(temp)
         for(let x = 0; x < temp.length; x++){
           newUserNames.set(temp[x].spotifyId, temp[x].username)
         }
@@ -95,43 +97,102 @@ export default function Feed({params} : {params: {id: string}}) {
       }
     }
   
-    // maniuplate the likes 
     const sendTheLikes = async (likedIndex : number) => {
-      // you liked to be true, so it changes the color of the button
-      // you have to call on the api to change the actual value in the database
-      // this is only called when the button is pressed
-      // you should only change the count based by the boolean
+      const newLikingInteraction = [...likingInteraction];
+      newLikingInteraction[likedIndex] = !newLikingInteraction[likedIndex];
+      setlikingInteraction(newLikingInteraction);
+      let postID = posts[likedIndex]._id
+      
+      if(newLikingInteraction[likedIndex]){
+        try{
+          let numberOfTheLikes = posts[likedIndex].likes + 1
+          const changingLike = await axios.put(`http://localhost:3000/api/feed/post/?id=${postID}&likes=${numberOfTheLikes}`);
+          console.log(changingLike)
+        }catch(error){
+          console.error("Unable to make the changes")
+        }
+      }else{
+        try{
+          let numberOfTheLikes = posts[likedIndex].likes - 1
+          const changingLike = await axios.put(`http://localhost:3000/api/feed/post/?id=${postID}&likes=${numberOfTheLikes}`);
+          console.log(changingLike)
+        }catch(error){
+          console.error("changed index")
+        }
+      }
     }
+
+    const getUserWebsite = async (userIndex: number) => {
+      let userIndexx = posts[userIndex].spotifyId;
+      window.location.href = `http://localhost:3000/profile/${userIndexx}`;
+    };
+
+    const addAcomment = async (theInfo: number) => {
+      let commentData = {
+        spotifyId: spotifyId,
+        numberOfLikes: 0,
+        content: theInfo,
+        createdAt: new Date(),
+        updatedAt: new Date() 
+      };
+
+      try{
+        const theChanges = await axios.post("/api/users/comments", {
+          body: commentData
+        });
+        console.log("Able to populate")
+      }
+      catch(error){
+        console.log("Oops we were unable to populate")
+      }
+    }
+  
+
+
     return (
-      <>
+      <div className="bg-black-200 min-h-screen py-8">
+        <div className="max-w-lg mx-auto">
           {ableFeed ? (
-              posts.map((post, index) => (
-                  <div key={index}>
-                      <div className = "theUser"> 
-                      {/* take the userNames and given the post.spotifyId, map it to it*/}
-                        <p>{userNames.get(post.spotifyId)}</p>
-                        <p>{post.updatedAt}</p>
-                        <p>{post.caption}</p>
-                      </div>
-                      <div className = "theSong"> 
-                        <p>{post.albumName}</p>
-                        <p>{post.songName}</p>
-                        <p>{post.caption}</p>
-                      </div> 
-                      <div className = "interactions">
-                        {/* here we want a like button that would then increase the count */}
-                        <div className = "theLikes"> 
-                          <button> LIKES </button>
-                          <p>{post.likes}</p>
-                        </div>
-                        <p>the comments here</p>
-                      </div>
+            posts.map((post, index) => (
+              <div className="bg-gray-500 rounded-md shadow-md p-4 mb-4">
+                <div className="flex items-start justify-between mb-2">
+                  {/* User Details */}
+                  <div className="flex items-center">
+                    <div className="flex flex-col">
+                      <button onClick = {() => getUserWebsite(index)}>{userNames.get(post.spotifyId)}</button>
+                      <p className="text-black-500 text-sm">{post.updatedAt}</p>
+                      <p>{post.caption}</p>
+                    </div>
                   </div>
-              ))
+                  {/* Image */}
+                  {/* on click it leads u to spotify */}
+                  <img src="album-image.jpg" alt="Album Image" className="w-20 h-20 rounded-md" />
+                  {/* Album Information */}
+                  <div className="flex flex-col items-end">
+                    <p>{post.albumName}</p>
+                    <p>{post.songName}</p>
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <button onClick={() => sendTheLikes(index)} className="bg-purple-500 text-white px-4 py-2 rounded-md mr-2">
+                      {likingInteraction[index] ? "Unlike" : "Like"}
+                    </button>
+                    <p>{post.likes}</p>
+                  </div>
+                  {/* figure out how isha did it and how to implement it  */}
+                  <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                    Comments
+                  </button>
+                </div>
+              </div>
+            ))
           ) : (
-              <>No Followers</>
+            <p>No Followers</p>
           )}
-      </>
-  );
+        </div>
+      </div>
+    );
     
 }
