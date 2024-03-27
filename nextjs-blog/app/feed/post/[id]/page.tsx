843e1f0e40de84b55eff948aebfddb53430f4908
Saba <sabayara0@gmail.com>
2024-03-27 01:29:52 -0600

    frontend for top track placements

--- nextjs-blog/app/feed/post/[id]/page.tsx
+++ nextjs-blog/app/feed/post/[id]/page.tsx
@@ -188,8 +188,6 @@ export default function PostPage({ params }: { params: { id: string } }) {
     }
   }
 
-  
-
   const handlePostSong = async (event: React.MouseEvent<HTMLButtonElement>) => {
     if (selectedSong) {
       try {
@@ -224,11 +222,6 @@ export default function PostPage({ params }: { params: { id: string } }) {
     setSelectedAlbum(null);
   };
 
-  // const handleOpenPopup = (song: Song) => {
-  //   setSelectedSong(song);
-  //   setShowPopup(true);
-  // };
-
   const handleClosePopup = () => {
     setSelectedSong(null);
     setShowPopup(false);
@@ -245,6 +238,7 @@ export default function PostPage({ params }: { params: { id: string } }) {
           onChange={handleInputChange}
         />
         {/* Button to go back */}
+
         <button
           onClick={handleBackToAlbums}
           className="mb-4 ml-4 w-10 h-10 bg-gray-500 text-white rounded-full hover:bg-gray-600 items-center justify-center focus:outline-none focus:ring-2 focus:text-black"
@@ -255,41 +249,54 @@ export default function PostPage({ params }: { params: { id: string } }) {
         </button>
       </div>
       <div className="flex flex-wrap justify-center">
-      {/* Top Songs */}
-        <div>
-        <h2 className="text-2xl font-bold mb-4">Top Songs</h2>
-        <ul>
-          {topSongs.map((song, index) => (
-            <li key={index} className="flex items-center space-x-4 mb-4">
-              <img src={song.image} alt={song.name} className="w-16 h-16 rounded-md" />
-              <div>
-                <div className="text-black">{song.name}</div>
-                <div>{song.artist}</div>
-                <div>{song.album}</div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
 
-        {/* Playlists */}
-        {showAlbums ? (
-          filteredAlbums.map((playlist, index) => (
-            <div key={index} className="m-4 cursor-pointer" onClick={() => {
-              fetchPlaylistSongs(playlist.id);
-              setSelectedAlbum(playlist); // Set the selected album when clicked
-              setSearchQuery(""); // Clear search query when album is selected
-            }}>
-              <img src={playlist.image} alt={playlist.name} className="w-40 h-40 object-cover rounded-lg mb-2" />
-              <p className="text-center">{playlist.name}</p>
+          {showAlbums && (
+          <>
+            {/* Top Songs */}
+            <div>
+              <h2 className="text-2xl text-black font-bold mb-4">Top Songs</h2>
+              <div className="grid grid-cols-2 gap-4">
+                {topSongs.slice(0, 6).map((song, index) => (
+                  <div key={index} className="flex items-center space-x-4 mb-4">
+                    <div className="font-bold text-xl text-gray-600">{index + 1}</div>
+                    <img src={song.image} alt={song.name} className="w-20 h-20 rounded-md" />
+                    <div>
+                      <div className="text-black font-bold">{song.name}</div>
+                      <div className="text-black">{song.artist}</div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-          ))
-        ) : (
+            
+            {/* Albums */}
+            <div className="flex flex-wrap justify-center items-center mt-8"> 
+
+              {filteredAlbums.map((playlist, index) => (
+                <div
+                  key={index}
+                  className="m-4 cursor-pointer flex-shrink-0"
+                  onClick={() => {
+                    fetchPlaylistSongs(playlist.id);
+                    setSelectedAlbum(playlist); // Set the selected album when clicked
+                    setSearchQuery(""); // Clear search query when album is selected
+                  }}
+                >
+                  <img src={playlist.image} alt={playlist.name} className="w-40 h-40 object-cover rounded-lg mb-2 " />
+                  <p className="text-center text-black" >{playlist.name}</p>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+        {!showAlbums && (
           // Render songs of selected album only
           <div className="flex flex-col items-center m-4">
+  
             {/* Render filtered songs based on search query */}
             <ul>
-              {playlistSongs.filter(song =>
+              {playlistSongs
+                .filter(song =>
                   song.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
                   song.artist.toLowerCase().includes(searchQuery.toLowerCase())
                 )
@@ -371,4 +378,151 @@ export default function PostPage({ params }: { params: { id: string } }) {
     </div>
   );
   
+
+  // return (
+  //   <div className="flex flex-col items-center m-4">
+  //     <div className="mb-4">
+  //       <input
+  //         type="text"
+  //         placeholder={`Search ${selectedAlbum ? selectedAlbum.name : 'Album'}...`}
+  //         className="bg-gray-200 mt-3 mr-4 pl-4 pr-8 py-1 rounded-full focus:outline-none focus:ring focus:border-blue-300 max-w-104 text-black"
+  //         value={searchQuery}
+  //         onChange={handleInputChange}
+  //       />
+  //       {/* Button to go back */}
+  //       <button
+  //         onClick={handleBackToAlbums}
+  //         className="mb-4 ml-4 w-10 h-10 bg-gray-500 text-white rounded-full hover:bg-gray-600 items-center justify-center focus:outline-none focus:ring-2 focus:text-black">
+  //         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block align-middle" viewBox="0 0 20 20" fill="currentColor">
+  //           <path fillRule="evenodd" d="M12.293 3.293a1 1 0 011.414 1.414L7.414 10l6.293 6.293a1 1 0 01-1.414 1.414l-7-7a1 1 0 010-1.414l7-7a1 1 0 011.414 0z" clipRule="evenodd" />
+  //         </svg>
+  //       </button>
+  //     </div>
+  //     <div className="flex flex-wrap justify-center">
+  //     <div>
+  // {/* Top Songs */}
+  //     <div>
+  //       <h2 className="text-2xl text-black font-bold mb-4">Top Songs</h2>
+  //       <div className="grid grid-cols-2 gap-4">
+  //         {topSongs.slice(0, 6).map((song, index) => ( // Slice to get only the first six songs
+  //           <div key={index} className="flex items-center space-x-4 mb-4">
+  //             <div className="font-bold text-xl text-gray-600">{index + 1}</div>
+  //             <img src={song.image} alt={song.name} className="w-20 h-20 rounded-md" />
+  //             <div>
+  //               <div className="text-black font-bold">{song.name}</div>
+  //               <div>{song.artist}</div>
+  //               <div>{song.album}</div>
+  //             </div>
+  //           </div>
+  //         ))}
+  //       </div>
+  //     </div>
+
+  //     {/* Playlists */}
+  //     {showAlbums && (
+  //       <div>
+  //         <h2 className="text-2xl text-black font-bold mb-4">Albums</h2>
+  //         <div className="flex flex-wrap justify-center">
+  //           {filteredAlbums.map((playlist, index) => (
+  //             <div key={index} className="m-4 cursor-pointer" onClick={() => {
+  //               fetchPlaylistSongs(playlist.id);
+  //               setSelectedAlbum(playlist); // Set the selected album when clicked
+  //               setSearchQuery(""); 
+  //             }}>
+  //               <img src={playlist.image} alt={playlist.name} className="w-40 h-40 object-cover rounded-lg mb-2" />
+  //               <p className="text-center">{playlist.name}</p>
+  //             </div>
+  //           ))}
+  //         </div>
+  //       </div>
+  //     )}
+  //   </div>
+
+  //       ) : (
+  //         // Render songs of selected album only
+  //         <div className="flex flex-col items-center m-4">
+  //           {/* Render filtered songs based on search query */}
+  //           <ul>
+  //             {playlistSongs.filter(song =>
+  //                 song.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+  //                 song.artist.toLowerCase().includes(searchQuery.toLowerCase())
+  //               )
+  //               .map((song, index) => (
+  //                 <li
+  //                   key={index}
+  //                   className={`cursor-pointer mb-2 flex items-center ${selectedSong === song ? 'bg-gray-300' : ''}`}
+  //                   onClick={() => {
+  //                     setSelectedSong(prevSong => (prevSong === song ? null : song));
+  //                   }}
+  //                 >
+  //                 {/* Post button */}
+  //                 {selectedSong === song && (
+  //                     <button
+  //                       className="mr-2 text-black focus:outline-none group-hover:opacity-100 transition-opacity"
+  //                       onClick={(e) => {
+  //                         e.stopPropagation();
+  //                         setShowPopup(true);
+  //                       }}
+  //                     >
+  //                       POST
+  //                     </button>
+  //                   )}
+  //                   <img src={song.image} alt={song.name} className="w-12 h-12 object-cover rounded-lg mr-2" />
+  //                   <div>
+  //                     <p className={`text-base font-semibold ${selectedSong === song ? 'text-gray-800' : ''}`}>{song.name}</p>
+  //                     <p className={`text-xs ${selectedSong === song ? 'text-gray-800' : ''}`}>{song.artist}</p>
+  //                   </div>
+  //                   {/* Play/pause button */}
+  //                   <button
+  //                     className="ml-auto mr-4 text-gray-600 focus:outline-none"
+  //                     onClick={(e) => {
+  //                       e.stopPropagation(); // Prevent the row selection event from triggering
+  //                       playPreview(song.audioUrl);
+  //                     }}
+  //                   >
+  //                     {/* SVG triangle icon for play button with rounder tips */}
+  //                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  //                       {selectedSong === song ? (
+  //                         // Three lines for pause
+  //                         <>
+  //                           <rect x="4" y="6" width="4" height="12" />
+  //                           <rect x="14" y="6" width="4" height="12" />
+  //                         </>
+  //                       ) : (
+  //                         // Play triangle with rounder tips
+  //                         <path  strokeLinejoin="round" strokeWidth={2} d="M4 6l16 6-16 6z" />
+  //                       )}
+  //                     </svg>
+  //                   </button>
+  //                 </li>
+  //               ))}
+  //           </ul>
+  
+  //           {/* Post song popup */}
+  //           {showPopup && (
+  //             <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center popup-background" onClick={handleClosePopup}>
+  //               <div className="bg-white p-4 rounded-lg shadow-lg" onClick={(e) => e.stopPropagation()}>
+  //                 <img src={selectedSong?.image} alt={selectedSong?.name} className="w-40 h-40 object-cover rounded-lg mb-2" />
+  //                 <p className="text-base font-semibold text-black mb-1">{selectedSong?.name}</p>
+  //                 <p className="text-sm text-black mb-2">{selectedSong?.artist}</p>
+  //                 <input
+  //                   type="text"
+  //                   placeholder="Enter caption..."
+  //                   className="border-b border-black text-black rounded-none px-2 py-1 w-full"
+  //                   value={caption}
+  //                   onChange={(e) => setCaption(e.target.value)}
+  //                 />
+  //                 <button onClick={handlePostSong} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mt-2">
+  //                   Post Song
+  //                 </button>
+  //               </div>
+  //             </div>
+  //           )}
+
+  //         </div>
+  //       )
+  //     </div>
+  //   </div>
+  // );
+
 }
